a0dfaf94076c96744b2401036ca813831c1eba0e
SirSonthonax <37802981+SirSonthonax@users.noreply.github.com>
2018-04-16 14:54:21 +0200

    Update map.js

--- map.js
+++ map.js
@@ -20,3 +20,33 @@ function preload() {
             mapwidth + "x" +
             mapheight + '?access_token='+
             api_key);
+
+  function setup() {
+  createCanvas(mapwidth, mapheight);
+  translate(width / 2, height / 2);
+  imageMode(CENTER);
+  image(mapimage,0,0);
+  currenttime =  earthquakedata.features[earthquakedata.features.length - 1].properties.time
+  endtime = earthquakedata.features[0].properties.time
+  for (i = 0; i<earthquakedata.features.length; i++){
+    data = earthquakedata.features[i];
+    let long = data.geometry.coordinates[0];
+    let lat = data.geometry.coordinates[1];
+    let x = mercX(long) - mercX(centerlongitude);
+    let y = mercY(lat) - mercY(centerlatitude);
+    
+    if(x < - width/2) {
+      x += width;
+    } else if(x > width / 2) {
+      x -= width;
+    }
+    earthquakedata.features[i].geometry.x = x
+    earthquakedata.features[i].geometry.y = y
+  }
+}
+
+function draw() {
+  
+  translate(width / 2, height / 2);
+  imageMode(CENTER);
+  image(mapimage,0,0);
